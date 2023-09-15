621d96980287269e0353e5ca3d86d1ee4dd9b18a
baotran310702 <tranbao1414@gmail.com>
2023-09-15 23:17:27 +0700

    update footer, slide

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,23 @@ import {
   sneaker,
   highlight,
   story,
+  footerAPI,
 } from "./data/data.js";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <main className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi} />
-      <Sales endpoint={popularsales} ifPopular />
-      <FeatureItems endpoint={highlight} ifPopular />
-      <Sales endpoint={topratesales} />
-      <FeatureItems endpoint={sneaker} />
-      <TopStories endpoint={story} />
-    </main>
+    <>
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifPopular />
+        <FeatureItems endpoint={highlight} ifPopular />
+        <Sales endpoint={topratesales} />
+        <FeatureItems endpoint={sneaker} />
+        <TopStories endpoint={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </>
   );
 };
 
