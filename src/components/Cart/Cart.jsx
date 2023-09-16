6144c843de256f3b0afc6aac3eb4be972602fb6f
baotran310702 <tranbao1414@gmail.com>
2023-09-16 15:58:19 +0700

    update new data

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cart;
