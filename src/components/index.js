96c9d4280677c54b42aa4cecb5a2f2399d7f8254
baotran310702 <tranbao1414@gmail.com>
2023-09-16 09:35:57 +0700

    create cart

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Hero } from "./Hero.jsx";
 export { default as Sales } from "./Sales.jsx";
 export { default as FeatureItems } from "./FeatureItems.jsx";
 export { default as TopStories } from "./TopStories.jsx";
+export { default as Cart } from "./Cart.jsx";
 export { default as Navbar } from "./Navbar.jsx";
