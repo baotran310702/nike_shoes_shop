11b62b2d6248ddb3d904f85ca582f19705dfa115
baotran310702 <tranbao1414@gmail.com>
2023-09-26 11:20:35 +0700

    update listshoes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Battery100Icon,
   BoltSlashIcon,
@@ -9,13 +9,43 @@ import {
   SunIcon,
 } from "@heroicons/react/24/solid";
 
-import flower from "../assets/flower.svg";
-
 const Sidebar = () => {
+  const [IsAtEnd, setIsAtEnd] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      // Calculate a threshold (e.g., 100 pixels) from the bottom
+      const threshold = 300;
+
+      if (scrollY + windowHeight + threshold >= documentHeight) {
+        setIsAtEnd(true);
+        console.log("at end");
+      } else {
+        setIsAtEnd(false);
+        console.log("not at end");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="relative left-0 top-10 ">
-        <div className="fixed lg:absolute grid grid-cols-1 lg:flex items-center justify-items-start w-1/5 lg:w-screen h-3/4 lg:h-16 md:h-12 gap-1 mt-12 p-2 shadow shadow-slate-600 text-slate-800 text-sm md:text-xs overflow-x-scroll scroll-smooth scroll-hidden">
+      <div className="relative left-0 top-10">
+        <div
+          className={`${
+            !IsAtEnd
+              ? "fixed grid grid-cols-1 lg:flex items-center justify-items-start w-full lg:w-screen h-auto lg:h-16 md:h-12 gap-1 mt-12 p-2 shadow shadow-slate-600 text-slate-800 overflow-x-scroll scroll-smooth scroll-hidden"
+              : "relative grid grid-cols-1 lg:flex items-center justify-items-start w-full lg:w-screen h-auto lg:h-16 md:h-12 gap-1 mt-12 p-2 shadow shadow-slate-600 text-slate-800 overflow-x-scroll scroll-smooth scroll-hidden"
+          }`}
+        >
           <div className="font-semibold text-xl flex justify-center items-center">
             <FireIcon className="w-6 h-6 mr-1" />
             Collections
--- src/user_pages/ListShoes.jsx
+++ src/user_pages/ListShoes.jsx
@@ -3,6 +3,9 @@ import { useDispatch } from "react-redux";
 import { setHiddenFooter } from "../app/FooterSlice";
 import { setHiddenNav, setStateNav } from "../app/NavbarSlice";
 import Sidebar from "../components/Sidebar";
+import { Sales } from "../components";
+import { topratesales } from "../data/data";
+import Items from "../components/utils/Items";
 
 const ListShoes = () => {
   const dispatch = useDispatch();
@@ -23,11 +26,34 @@ const ListShoes = () => {
       })
     );
   }, []);
+  console.log(topratesales);
+
+  const ifPopular = false;
 
   return (
     <>
-      <div className="relative h-[120vh] bg-white">
-        <Sidebar />
+      <div className="relative flex flex-row bg-white">
+        <div className="absolute sidebar w-1/5 h-auto">
+          <Sidebar />
+        </div>
+        <div className="absolute w-4/5 bg-blue-400 h-screen top-20 right-0 gap-0">
+          <div>Category</div>
+          <div className="items">
+            <div className="nike-container">
+              <div
+                className={`grid items-center justify-items-center ${
+                  ifPopular
+                    ? "grid-cols-3 xl:grid-cols-2 md:grid-cols-1 "
+                    : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 "
+                } gap-7 lg:gap-5 mt-7`}
+              >
+                {topratesales.items?.map((item, i) => (
+                  <Items {...item} key={i} ifPopular={ifPopular} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
